b59fbdb74a79f5079cbdab28de2db4fe11b92a76
prehooo <kamilnatonek@gmail.com>
2020-03-29 23:20:04 +0200

    Updates

---
+++ precache-manifest.34b69adb3f5833a9246a4292275bec53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b995889db9bedc22f8636434a636c49d",
+    "url": "/Kantor/index.html"
+  },
+  {
+    "revision": "9e049802d75cde1710f5",
+    "url": "/Kantor/static/css/main.2c917a5f.chunk.css"
+  },
+  {
+    "revision": "7b91f19b8cfcedf3af2a",
+    "url": "/Kantor/static/js/2.eafee64e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kantor/static/js/2.eafee64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e049802d75cde1710f5",
+    "url": "/Kantor/static/js/main.bfebd682.chunk.js"
+  },
+  {
+    "revision": "eb656ebf64211364c603",
+    "url": "/Kantor/static/js/runtime-main.f407664b.js"
+  }
+]);
